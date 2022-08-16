61b943def42fb1eb02a395d83b41159c6d03f35b
Yaswho <noyasmim17@gmail.com>
2022-08-16 00:19:03 -0300

    Atualização tabela de pacientes

--- models/Paciente.js
+++ models/Paciente.js
@@ -1,13 +1,26 @@
 const sequelize = require('sequelize');
+const { DOUBLE } = require('sequelize');
 const { Sequelize } = require('sequelize');
 const db = require('./db');
 
-const Paciente = db.define('nome_do_paciente', {
+const Paciente = db.define('Pacientes', {
     id:{
         type: Sequelize.INTEGER,
         autoIncrement: true,
         allowNull: false,
         primaryKey: true
+    },
+    idade:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    altura:{
+        type: Sequelize.DOUBLE,
+        allowNull: false,     
+    },
+    peso:{
+        type: Sequelize.DOUBLE,
+        allowNull: false,     
     },
     nome_paciente:{
         type: Sequelize.STRING,
@@ -16,8 +29,12 @@ const Paciente = db.define('nome_do_paciente', {
     email:{
         type: Sequelize.STRING,
         allowNull: false,
-    }
-
+    },
+    sexo:{
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    
 });
 //Criar tabela 
 Paciente.sync();
