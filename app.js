c881d03db459e317968966f822eb09f264f87d3b
Yaswho <noyasmim17@gmail.com>
2022-08-16 00:35:02 -0300

    Verificação de email

--- app.js
+++ app.js
@@ -11,7 +11,20 @@ app.get("/", async(req, res)=> {
 });
 
 app.post("/registrarPaciente", async(req, res)=> {
-    //console.log(req.body);
+   
+    const users = await Paciente.findAll({
+        where: {
+          email: req.body.email
+         }
+        }); 
+        
+    if(Object.keys(users).length > 0) {
+        return res.status(400).json({
+                erro: true,
+                mensagem: "Erro: Email já cadastrado!"
+            })
+        }
+
     await Paciente.create(req.body)
     .then(()=> {
         return res.json({
@@ -24,6 +37,8 @@ app.post("/registrarPaciente", async(req, res)=> {
             erro: true,
             mensagem: "Erro: Paciente não cadastrado!"
         })
+
+    
 
     });
 
